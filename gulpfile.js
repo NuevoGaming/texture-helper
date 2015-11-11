069c08190bc8930d2bb1481b0601698681a9a476
Anton <towa48@gmail.com>
2015-11-11 23:19:01 +0300

    Fix all compile-time errors;

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,40 @@ var gulp = require('gulp'),
     ts = require('gulp-typescript'),
     del = require('del');
 
-var paths = {
-    scripts: ['src/**/*.ts'],
-    target: 'dist'
+var PATHS = {
+    src: {
+        ts: 'src/**/*.ts'
+    },
+    target: 'dist',
+    typings: {
+        angular2: 'node_modules/angular2/bundles/typings/angular2/angular2.d.ts',
+        router: 'node_modules/angular2/bundles/typings/angular2/router.d.ts',
+        http: 'node_modules/angular2/bundles/typings/angular2/http.d.ts'
+    }
 };
 
 gulp.task('clean', function(next) {
-    del([paths.target]).then(function() {
+    del([PATHS.target]).then(function() {
         next();
     });
 });
 
 gulp.task('build', ['clean'], function() {
-    return gulp.src(paths.scripts)
-        .pipe(ts({
-            noImplicitAny: true,
-            removeComments: true,
-            target: 'ES5',
-            experimentalDecorators: true
-        }))
-        .pipe(gulp.dest(paths.target));
+    var tsResult = gulp.src([
+        PATHS.src.ts,
+        PATHS.typings.angular2,
+        PATHS.typings.router,
+        PATHS.typings.http
+    ])
+    .pipe(ts({
+        noImplicitAny: true,
+        module: 'commonjs',
+        target: 'ES5',
+        emitDecoratorMetadata: true,
+        experimentalDecorators: true
+    }));
+
+    return tsResult.js.pipe(gulp.dest(PATHS.target));
 });
 
 gulp.task('default', ['build'], function() {
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,5 +1,3 @@
-/// <reference path="../node_modules/angular2/angular2.d.ts" />
-
 import { bootstrap, FORM_PROVIDERS } from 'angular2/angular2';
 import { ROUTER_PROVIDERS } from 'angular2/router';
 import { HTTP_PROVIDERS } from 'angular2/http';
