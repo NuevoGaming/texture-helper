4b81ce199b62890bc202d8abe742d1599074c930
Anton <towa48@gmail.com>
2015-11-18 22:07:33 +0300

    fix angular2 template refreshing;

--- src/welcome/app.ts
+++ src/welcome/app.ts
@@ -1,30 +1,29 @@
 /// <reference path="../../manual_typings/electron.d.ts"/>
 
-import { Component, View, NgClass } from 'angular2/angular2';
+import { Component, View, CORE_DIRECTIVES, FORM_DIRECTIVES } from 'angular2/angular2';
 import ipc = require('ipc');
 
 enum WindowState {
   NewProject = 1,
   ProjectType = 2
-};
+}
 
 @Component({
     selector: 'app'
 })
 @View({
     templateUrl: '../static/partials/welcome-app.html',
-    directives: [ NgClass ]
+    directives: [ CORE_DIRECTIVES, FORM_DIRECTIVES ]
 })
 export class App {
-    public state: WindowState;
+    state: WindowState;
     constructor() {
       this.state = WindowState.NewProject;
     }
     showOpenProjectDialog() {
-      console.log('showOpenProjectDialog');
       ipc.send('showOpenProjectDialog');
     }
     showProjectTypeSelector() {
-      this.state = WindowState.ProjectType
+      this.state = WindowState.ProjectType;
     }
 }
